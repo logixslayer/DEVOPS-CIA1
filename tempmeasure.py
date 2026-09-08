269e067d67c8a9d8afedc0f43e8580a5144f6b44
Aditya <ghatageadi07@gmail.com>
2026-09-08 10:44:18 +0530

    the first commit

---
+++ tempmeasure.py
@@ -0,0 +1,7 @@
+temp = int(input("Enter the temperature "))
+if (temp>50):
+    print("the given temp is hot")
+elif (temp <50 and temp>30):
+    print("the given temp is normal") 
+else :
+    print("the given temperature is cold")
